62050dc12930497675bef458a5004d3385fccda6
JEMBA <lordash991@gmail.com>
2026-07-03 19:55:43 -0700

    Agregar Config Manager

---
+++ jemba_core/utils/config_manager.py
@@ -0,0 +1,27 @@
+from config.settings import *
+
+class ConfigManager:
+
+    @staticmethod
+    def app_name():
+        return APP_NAME
+
+    @staticmethod
+    def symbol():
+        return DEFAULT_SYMBOL
+
+    @staticmethod
+    def interval():
+        return DEFAULT_INTERVAL
+
+    @staticmethod
+    def database():
+        return DATABASE_NAME
+
+    @staticmethod
+    def api_key():
+        return BINGX_API_KEY
+
+    @staticmethod
+    def secret():
+        return BINGX_SECRET
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from jemba_core.logging.logger import logger
+from jemba_core.utils.config_manager import ConfigManager
 
-logger.info("Motor iniciado correctamente.")
+logger.info(f"Proyecto : {ConfigManager.app_name()}")
+logger.info(f"Activo   : {ConfigManager.symbol()}")
+logger.info(f"Intervalo: {ConfigManager.interval()}")
+logger.info(f"Base DB  : {ConfigManager.database()}")
