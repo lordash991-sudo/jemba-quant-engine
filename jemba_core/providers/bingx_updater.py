f8556d8fdb1a92c4ae6aab24e64543e8b1bbcc2d
JEMBA <lordash991@gmail.com>
2026-07-04 16:30:33 -0700

    Agregar BingXUpdater

---
+++ jemba_core/providers/bingx_updater.py
@@ -0,0 +1,18 @@
+﻿from jemba_core.providers.bingx_provider import BingXProvider
+
+
+class BingXUpdater:
+
+    def __init__(self):
+        self.provider = BingXProvider()
+
+    def latest_closed_candle(self, symbol="BTC-USDT", timeframe="1h"):
+        candles = self.provider.get_candles(symbol, timeframe, limit=3)
+
+        if not candles:
+            return None
+
+        if len(candles) >= 2:
+            return candles[-2]
+
+        return candles[-1]
