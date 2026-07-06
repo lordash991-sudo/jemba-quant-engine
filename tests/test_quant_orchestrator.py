ed7ac95ecd69e7a9d2479c57d1aae63f295a347d
JEMBA <lordash991@gmail.com>
2026-07-06 11:57:11 -0700

    test(engine): agregar pruebas base QuantOrchestrator

---
+++ tests/test_quant_orchestrator.py
@@ -0,0 +1,21 @@
+﻿from jemba_core.engine.quant_orchestrator import QuantOrchestrator
+
+
+def test_quant_orchestrator_can_initialize():
+    q = QuantOrchestrator(
+        account_balance=10000,
+        min_confidence=0.70,
+    )
+
+    assert q is not None
+
+
+def test_quant_orchestrator_best_opportunity_empty():
+    q = QuantOrchestrator(
+        account_balance=10000,
+        min_confidence=0.70,
+    )
+
+    result = q.best_opportunity([])
+
+    assert result is None
