82804d4248defa6cb169206b26e157bb50bbd09d
JEMBA <lordash991@gmail.com>
2026-07-14 21:50:33 -0700

    fix(portfolio): corregir exports de portfolio_optimizer

--- jemba_core/portfolio_optimizer/__init__.py
+++ jemba_core/portfolio_optimizer/__init__.py
@@ -1,9 +1,15 @@
-from .fixed_fractional import fixed_fractional_size
-from .kelly import kelly_fraction
-from .volatility_sizing import volatility_position_size
+from jemba_core.portfolio_optimizer.fixed_fractional import (
+    fixed_fractional_position_size,
+    fixed_fractional_risk_amount,
+)
+from jemba_core.portfolio_optimizer.kelly import kelly_fraction
+from jemba_core.portfolio_optimizer.volatility_sizing import (
+    volatility_position_size,
+)
 
 __all__ = [
-    "fixed_fractional_size",
+    "fixed_fractional_position_size",
+    "fixed_fractional_risk_amount",
     "kelly_fraction",
     "volatility_position_size",
 ]
