1595a13309846236a2f37afc55e9aa7f1522579c
JEMBA <lordash991@gmail.com>
2026-07-17 15:42:12 -0700

    feat(trade-manager): integrar modulos del Trade Manager

--- jemba_core/trade_manager/__init__.py
+++ jemba_core/trade_manager/__init__.py
@@ -1,11 +1,31 @@
+from jemba_core.trade_manager.break_even import (
+    move_stop_to_break_even,
+)
 from jemba_core.trade_manager.orchestrator import (
     TradeAction,
     TradeDecision,
     TradeManager,
 )
+from jemba_core.trade_manager.partial_take_profit import (
+    PartialTakeProfitResult,
+    check_partial_take_profit,
+)
+from jemba_core.trade_manager.time_stop import (
+    TimeStopResult,
+    check_time_stop,
+)
+from jemba_core.trade_manager.trailing_manager import (
+    update_trailing_stop,
+)
 
 __all__ = [
+    "PartialTakeProfitResult",
+    "TimeStopResult",
     "TradeAction",
     "TradeDecision",
     "TradeManager",
+    "check_partial_take_profit",
+    "check_time_stop",
+    "move_stop_to_break_even",
+    "update_trailing_stop",
 ]
